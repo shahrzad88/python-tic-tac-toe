560bad566b43c3276f75345daf8380467e9be297
Shahrzad Masoumnia <shahrzad@spiretrading.com>
2018-01-12 14:06:32 -0500

    Works!

--- game.py
+++ game.py
@@ -7,60 +7,57 @@ class Board:
     self.height = height
     self.positions = [[' ' for i in range(width)] for j in range(height)]
     self.turn = None
+    self.last_move = None
   
   def is_empty(self):
-    sum = 0
-    for i in range(width):
-      for j in range(height):
-        if self.positions[i][j] == 'X' or self.positions[i][j] == 'O':
-          sum += 1
-    if sum == 0:
-      return True
-    else:
-      return False
+    for row in self.positions:
+      for cell in row:
+        if cell != " ":
+          return False
+    return True
 
   def is_full(self):
-    sum = 0
-    for i in range(self.width):
-      for j in range(self.height):
-        if self.positions[i][j] == 'X' or self.positions[i][j] == 'O':
-          sum += 1
-    if sum == self.width * self.height:
-      return True
-    else:
+    for row in self.positions:
+      for cell in row:
+        if cell == " ":
+          return False
+    print ('board is full')
+    return True
+
+  def is_horizontal_row(self):
+    if self.is_empty():
       return False
-
-  def get_cell_value(self, x, y):
-    return self.positions[x][y]
-
-  def is_horizontal_row(self, x, y):
-    cell_value = self.positions[x][y]
     sum = 0
+    cell_value = self.positions[self.last_move[0]][self.last_move[1]]
     for j in range(self.height):
-      if self.positions[x][j] == cell_value:
+      if self.positions[self.last_move[0]][j] == cell_value:
         sum += 1
     return sum == 3
 
-  def is_vertical_row(self, x, y):
-    cell_value = self.positions[x][y]
+  def is_vertical_row(self):
+    if self.is_empty():
+      return False
+    cell_value = self.positions[self.last_move[0]][self.last_move[1]]
     sum = 0
     for i in range(self.width):
-      if self.positions[x][y] == cell_value:
+      if self.positions[i][self.last_move[1]] == cell_value:
         sum += 1
     return sum == 3
 
-  def is_diagonal_row(self, x, y):
-    cell_value = self.positions[x][y]
-    sum = 0
-    if 
+  def is_diagonal_row(self):
+    if self.positions[1][1] == " ":
+      return False
+    return (self.positions[0][0] == self.positions[1][1] == \
+      self.positions[2][2]) or (self.positions[0][2] == \
+      self.positions[1][1] == self.positions[2][0])
 
+  def is_finished(self):
+    return self.is_diagonal_row() or self.is_vertical_row() or \
+    self.is_horizontal_row() or self.is_full()
 
-  def update(self, position):
-    temp_positions = position.split(',')
-    position_x = int(temp_positions[0])
-    position_y = int(temp_positions[1])
-    self.positions[position_x][position_y] = self.turn
-  
+  def update(self):
+    self.positions[self.last_move[0]][self.last_move[1]] = self.turn
+
   def draw(self):
     for row in self.positions:
       s = ''
@@ -75,14 +72,10 @@ class Board:
       self.turn = 'O'
 
 class Player:
-  def __init__(self, name, symbol):
+  def __init__(self, name):
     self.name = name
-    self.symbol = symbol
     self.points = 0
     self.turn = False
-
-  def increase_points(self):
-    self.points += 1
 
 def check_first_player():
   first_player = input('Who is starting the game (X or O)? ').upper().replace(
@@ -113,24 +106,35 @@ def ask_move(player_x, player_o, turn):
   except Exception as e:
     print (e)
     ask_move(prop_player_x, prop_player_o, prop_turn)
+  temp_position = position.split(',')
+  try:
+    position = list(map(int, temp_position))
+  except Exception as e:
+    print (e)
   return position
 
 def main():
   player_x = None
   player_o = None
-  turn = None
   #temp_dimensions = input('Enter board dimensions: ').split(',')
   width = 3 #int(temp_dimensions[0])
   height = 3 # int(temp_dimensions[1])
   board = Board(width, height)
-  player_x = Player('k', 'X') #input('X player, What is your name? '), 'X')
-  player_o = Player('s', 'O') #input('O player, What is your name? '), 'O')
+  player_x = Player('s') #input('X player, What is your name? '), 'X')
+  player_o = Player('k') #input('O player, What is your name? '), 'O')
   board.turn = check_first_player()
-  while board.is_full() == False:
-    position = ask_move(player_x, player_o, board.turn)
-    board.update(position)
+  while not board.is_finished():
+    board.last_move = ask_move(player_x, player_o, board.turn)
+    board.update()
     board.draw()
     board.change_turn()
+  if board.is_full():
+    print ("It's a tie!")
+  else:
+    if board.turn == 'X':
+      print (player_o.name + ' won!')
+    else:
+      print (player_x.name + ' won!')
 
 if __name__ == "__main__":
   main()
